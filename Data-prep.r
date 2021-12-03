18c9709b458e521b74a2ad6fc3ce8f338f6a59df
Karthik Balasubramanian <kartbala@users.noreply.github.com>
2021-12-03 14:12:02 -0500

    Update Data-prep.r

--- Data-prep.r
+++ Data-prep.r
@@ -8,15 +8,15 @@ options(digits=4)
  
 ## * Load data
 
-Daily_tweets <- read_sheet("https://docs.google.com/spreadsheets/d/1Nmn1_sS8a-JG_XBeNvlUwIv2vP_pV7IxplEapdOdsps/edit#gid=2129984587",sheet="daily.tweets")
-Covid <- read_sheet("https://docs.google.com/spreadsheets/d/1Nmn1_sS8a-JG_XBeNvlUwIv2vP_pV7IxplEapdOdsps/edit#gid=2129984587",sheet="WHO-COVID-19-global-data")
-Oil <- read_sheet("https://docs.google.com/spreadsheets/d/1Nmn1_sS8a-JG_XBeNvlUwIv2vP_pV7IxplEapdOdsps/edit#gid=2129984587",sheet="Brent Oil Prices")
-Search_term <- read_sheet("https://docs.google.com/spreadsheets/d/1Nmn1_sS8a-JG_XBeNvlUwIv2vP_pV7IxplEapdOdsps/edit#gid=2129984587",sheet="iran search term")
-Daily_events <- read_sheet("https://docs.google.com/spreadsheets/d/1Nmn1_sS8a-JG_XBeNvlUwIv2vP_pV7IxplEapdOdsps/edit#gid=2129984587",sheet="daily.events")
-Google_trends <- read_sheet("https://docs.google.com/spreadsheets/d/1Nmn1_sS8a-JG_XBeNvlUwIv2vP_pV7IxplEapdOdsps/edit#gid=2129984587",sheet="gtrendsweekly")
-Forex <- read_sheet("https://docs.google.com/spreadsheets/d/1Nmn1_sS8a-JG_XBeNvlUwIv2vP_pV7IxplEapdOdsps/edit#gid=2129984587",sheet="Forex")
-Weather <- read_sheet("https://docs.google.com/spreadsheets/d/1Nmn1_sS8a-JG_XBeNvlUwIv2vP_pV7IxplEapdOdsps/edit#gid=2129984587",sheet="weather")
+Daily_tweets <- read_sheet("https://docs.google.com/spreadsheets/d/1Nmn1_sS8a-JG_XBeNvlUwIv2vP_pV7IxplEapdOdsps/edit#gid=2129984587",sheet="daily.tweets") %>% data.table
+Covid <- read_sheet("https://docs.google.com/spreadsheets/d/1Nmn1_sS8a-JG_XBeNvlUwIv2vP_pV7IxplEapdOdsps/edit#gid=2129984587",sheet="WHO-COVID-19-global-data") %>% data.table 
+Oil <- read_sheet("https://docs.google.com/spreadsheets/d/1Nmn1_sS8a-JG_XBeNvlUwIv2vP_pV7IxplEapdOdsps/edit#gid=2129984587",sheet="Brent Oil Prices") %>% data.table
+Search_term <- read_sheet("https://docs.google.com/spreadsheets/d/1Nmn1_sS8a-JG_XBeNvlUwIv2vP_pV7IxplEapdOdsps/edit#gid=2129984587",sheet="iran search term") %>% data.table
+Daily_events <- read_sheet("https://docs.google.com/spreadsheets/d/1Nmn1_sS8a-JG_XBeNvlUwIv2vP_pV7IxplEapdOdsps/edit#gid=2129984587",sheet="daily.events") %>% data.table
+Google_trends <- read_sheet("https://docs.google.com/spreadsheets/d/1Nmn1_sS8a-JG_XBeNvlUwIv2vP_pV7IxplEapdOdsps/edit#gid=2129984587",sheet="gtrendsweekly") %>% data.table
+Forex <- read_sheet("https://docs.google.com/spreadsheets/d/1Nmn1_sS8a-JG_XBeNvlUwIv2vP_pV7IxplEapdOdsps/edit#gid=2129984587",sheet="Forex") %>% data.table
+Weather <- read_sheet("https://docs.google.com/spreadsheets/d/1Nmn1_sS8a-JG_XBeNvlUwIv2vP_pV7IxplEapdOdsps/edit#gid=2129984587",sheet="weather") %>% data.table
 
-Combined = Covid
+Combined = Daily_tweets %>% merge(Covid[Country_code=="IR",],by="Date") %>% merge(Oil,by="Date") %>% merge(Forex,by="Date")  %>% merge(Daily_events,by="Date")
 
 write.csv(Combined,"InCombined.csv")
