2329360723cec7f8505eac8d0f2538145062e6f3
Karthik Balasubramanian <kartbala@users.noreply.github.com>
2022-01-05 13:39:13 -0500

    Update Data-prep.r

--- Data-prep.r
+++ Data-prep.r
@@ -5,6 +5,7 @@ library(googlesheets4)
 library(googledrive)
 library(tidyverse)
 options(digits=4)
+setwd("~/Google\ Drive/")
 
 ## * Load data
 
@@ -12,15 +13,25 @@ google_workbook_name = "https://docs.google.com/spreadsheets/d/1Nmn1_sS8a-JG_XBe
 sheet_names = sheet_names(google_workbook_name)
 number_of_sheets = sheet_names %>% length
 
-#exclude Dictionary worksheet (last sheet)
+# Exclude Dictionary worksheet (last sheet)
 number_of_sheets = number_of_sheets - 1
 
+# Make a data table for every sheet other than Dictionary
 Data_table_maker = function(sheetnum){
 assign(sheet_names[sheetnum],data.table(read_sheet(google_workbook_name,sheet=sheet_names[sheetnum])),envir = .GlobalEnv)
 return(sheet_names[sheetnum])}
-
 sapply(1:number_of_sheets,Data_table_maker)
 
-Combined = list(daily.tweets,covid.daily.tweets,noncovid.daily.tweets,english.daily.tweets,no.farsi.daily.tweets,covid,oil,daily.events,protest.events,repression.events,forex,weather) %>% reduce(left_join, by = "Date")
+# Make a list of data tables
+data_table_list = NULL
 
-write.csv(Combined,"InCombined.csv")
+Data_table_lister = function(sheetnum){
+data_table_list <<- list(data_table_list,get(sheet_names[sheetnum]))
+return()}
+sapply(1:number_of_sheets,Data_table_lister)
+
+# Merge all of the data tables
+Combined = list(daily.tweets,covid.daily.tweets,noncovid.daily.tweets,english.daily.tweets,no.farsi.daily.tweets,covid,oil,daily.events,protest.events,repression.events,weather) %>% reduce(left_join, by = "Date")
+
+# Write file to Google Drive
+write.csv(Combined,file="InCombined.csv")
