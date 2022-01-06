4cf5a5e586879e6ecf815e4220ef592aaba3b95f
Karthik Balasubramanian <kartbala@users.noreply.github.com>
2022-01-05 20:31:23 -0500

    Update Data-prep.r

--- Data-prep.r
+++ Data-prep.r
@@ -31,8 +31,8 @@ return()}
 sapply(1:number_of_sheets,Data_table_lister)
 
 # Merge all of the data tables
-Combined = list(daily.tweets,covid.daily.tweets,noncovid.daily.tweets,english.daily.tweets,no.farsi.daily.tweets,covid,oil,daily.events,protest.events,repression.events,weather) %>% reduce(left_join, by = "Date")
-#a = list(get(sheet_names[1]),get(sheet_names[2]))
+l.dt <- lapply(sheet_names[1:number_of_sheets], function(x) get(x))
+Combined = l.dt %>% reduce(left_join, by = "Date")
 
 # Write file to Google Drive
 write.csv(Combined,file="InCombined.csv")
